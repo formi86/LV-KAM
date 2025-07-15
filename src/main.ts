9b270297e4f8762c2355f40c35cc0860f1fcc924
formi86 <nicformichelli@gmail.com>
2025-07-15 15:30:30 -0300

    build works

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+import 'vuetify/lib/styles/main.sass'
 
 const vuetify = createVuetify({
   components,
